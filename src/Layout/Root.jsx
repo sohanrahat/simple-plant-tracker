674168b30577289b1562aff32c077bbc9228b398
sohanrahat <sohanboy@gmail.com>
2025-05-20 16:32:57 +0600

    basic layout setup done

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import { Outlet } from 'react-router';
 
 const Root = () => {
     return (
         <div>
-
+            <nav>
+                <Navbar />
+            </nav>
+            <main>
+                <Outlet />
+            </main>
+            <footer>
+                <Footer />
+            </footer>
         </div>
     );
 };
