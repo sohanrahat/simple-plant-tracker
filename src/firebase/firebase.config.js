78a433368d3758813aa0a0927183e8563baed024
sohanrahat <sohanboy@gmail.com>
2025-05-21 19:44:58 +0600

    firebase SDK

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAXDtieOJhg-AopP6z6fn_OZjvfFYT1AjQ",
+    authDomain: "plantplanet-1d750.firebaseapp.com",
+    projectId: "plantplanet-1d750",
+    storageBucket: "plantplanet-1d750.firebasestorage.app",
+    messagingSenderId: "478528224709",
+    appId: "1:478528224709:web:717cfa221b8e55a9dc5fac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app; 
